cd4cbccdcd9c77404edb4a4bf430889b9556dc9d
Mahdi Razzaghi <103171417+MahdiRazzaghi808@users.noreply.github.com>
2023-07-15 23:47:41 +0330

    first config to match

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    screens:{
-      'sm-x3':{"max":"390px"}, 
-      'sm-x2':{"max":"500px"},
-      'sm':{"max":"767px"},
-      'md':{"max":"900px"},
-      'lg':{"max":"1200px"},
-      'lg-x2':{"min":"1201px"},
-     
+    screens: {
+      'sm-x3': { "max": "390px" },
+      'sm-x2': { "max": "500px" },
+      'sm': { "max": "767px" },
+      'md': { "max": "900px" },
+      'lg': { "max": "1200px" },
+      'lg-x2': { "min": "1201px" },
+
     },
-    borderWidth:{
-      '1': '1px',
+    fontFamily: {
+      "vazirBold": 'vazirBold',
+      "vazirMedium": 'vazirMedium',
+      "vazirLight": 'vazirLight'
     },
-    fontFamily:{
-      "vazirBold" : 'vazirBold',
-      "vazirMedium" : 'vazirMedium',
-      "vazirLight" : 'vazirLight'
+    extend: {
+      colors: {
+        'secondText': '#8d8d8d',
+        'golden': '#ffc542',
+        'hederBg': '#18191d',
+        'mainBg': '#111217'
+      }
     },
-    extend: {},
   },
   plugins: [],
 }
