35a1ae62f4e982320a118714fd6b3fc691faa190
KasraMg <kasragithub7@gmail.com>
2023-07-16 14:55:10 +0330

    darkMode

--- JavaScript/utilis/shared.js
+++ JavaScript/utilis/shared.js
@@ -6,7 +6,9 @@ toggle.addEventListener('click',()=>{
     darkMode= !darkMode
     if (darkMode) {
         document.documentElement.classList.add('dark')
+        toggle.style.background='#fff'
     }else{
-        document.documentElement.classList.remove('dark')
+        document.documentElement.classList.remove('dark') 
+        toggle.style.background='#000'
     }
 })
