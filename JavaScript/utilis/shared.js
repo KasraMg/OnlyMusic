f8d640b3bafa26ce82ae694ccf71ceed53d87e86
Mahdi Razzaghi <103171417+MahdiRazzaghi808@users.noreply.github.com>
2023-07-17 00:55:50 +0330

    fix bug dark-mode

--- JavaScript/utilis/shared.js
+++ JavaScript/utilis/shared.js
@@ -1,33 +1,58 @@
-let darkMode=true
-const Alltoggle =document.querySelectorAll('#toggle')
+let darkMode = false;
+const AllToggle = document.querySelectorAll('#toggle');
+const toggleBtn = document.querySelectorAll('#dark-mode');
 
 
+window.addEventListener('load', () => {
+    if (!localStorage.getItem('darkMode')) {
+        localStorage.setItem('darkMode', 'dark');
+        darkMode = 'dark'
+    } else {
+        darkMode = localStorage.getItem('darkMode');
+        console.log(darkMode);
+    }
 
-window.addEventListener('load',()=>{
-    if (darkMode) {
+    if (darkMode==='dark') {
         document.documentElement.classList.add('dark')
-        Alltoggle[0].style.background='#fff'
-        Alltoggle[1].style.background='#fff'
-    }else{
-        document.documentElement.classList.remove('dark') 
-        Alltoggle[0].style.background='#000'
-        Alltoggle[1].style.background='#000'
+        AllToggle[0].style.background = '#fff'
+        AllToggle[1].style.background = '#fff'
+    } else {
+        document.documentElement.classList.remove('dark')
+        AllToggle[0].style.background = '#000'
+        AllToggle[1].style.background = '#000';
+        toggleBtn[0].checked = true
+        toggleBtn[1].checked = true
     }
 })
 
-Alltoggle.forEach(toggle=>{
-    toggle.addEventListener('click',()=>{
-    darkMode= !darkMode
-    if (darkMode) {
-        document.documentElement.classList.add('dark')
-        toggle.style.background='#fff'
-    }else{
-        document.documentElement.classList.remove('dark') 
-        toggle.style.background='#000'
-    }
-    console.log(darkMode);
-})
+
+
+
+AllToggle.forEach((toggle,index) => {
+    toggle.addEventListener('click', ()=>{
+    
+        if (darkMode === 'dark') {
+            darkMode = 'light';
+            localStorage.setItem('darkMode', darkMode);
+        } else {
+            darkMode = 'dark';
+            localStorage.setItem('darkMode', darkMode);
+        }
+
+        if(darkMode==='dark'){
+            document.documentElement.classList.add('dark')
+            AllToggle[0].style.background = '#fff'
+            AllToggle[1].style.background = '#fff' 
+            toggleBtn[1].checked = false
+                      }else{
+                        document.documentElement.classList.remove('dark')
+                        AllToggle[0].style.background = '#000'
+                        AllToggle[1].style.background = '#000';
+                        toggleBtn[1].checked = true
+                      }
+        
+       })
+    
 })
 
 
- 
