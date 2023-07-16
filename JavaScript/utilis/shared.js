32dbaf2b6dc230af632568492d6980e0f364cab9
KasraMg <kasragithub7@gmail.com>
2023-07-16 19:53:36 +0330

    responsiveHeader

--- JavaScript/utilis/shared.js
+++ JavaScript/utilis/shared.js
@@ -1,6 +1,18 @@
-let darkMode=false
-
+let darkMode=true
 const toggle =document.querySelector('#toggle')
+
+
+
+window.addEventListener('load',()=>{
+    if (darkMode) {
+        document.documentElement.classList.add('dark')
+        toggle.style.background='#fff'
+    }else{
+        document.documentElement.classList.remove('dark') 
+        toggle.style.background='#000'
+    }
+})
+
 
 toggle.addEventListener('click',()=>{
     darkMode= !darkMode
