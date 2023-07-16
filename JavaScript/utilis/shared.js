536895f820806ed904a39cbb3d71102176d842ab
KasraMg <kasragithub7@gmail.com>
2023-07-16 20:09:00 +0330

    asideJs

--- JavaScript/utilis/shared.js
+++ JavaScript/utilis/shared.js
@@ -25,3 +25,35 @@ toggle.addEventListener('click',()=>{
     }
     console.log(darkMode);
 })
+
+const aside = document.querySelectorAll('aside a');
+aside.forEach(item => {
+    item.addEventListener('click', event => {
+        aside.forEach(item => {
+            item.classList.remove('text-white')
+            item.classList.remove('bg-redBg');
+            item.classList.remove('dark:text-mainBg')
+            item.classList.remove('dark:bg-golden')
+
+        })
+
+        const clickItem = event.target
+
+        if (clickItem.parentNode.tagName === 'A') {
+
+            clickItem.parentNode.classList.add('text-white');
+            clickItem.parentNode.classList.add('bg-redBg');
+            clickItem.parentNode.classList.add('dark:text-mainBg');
+            clickItem.parentNode.classList.add('dark:bg-golden');
+
+        } else {
+            clickItem.classList.add('text-white');
+            clickItem.classList.add('bg-redBg')
+            clickItem.classList.add('dark:text-mainBg');
+            clickItem.classList.add('dark:bg-golden');
+
+        }
+
+
+    })
+})
