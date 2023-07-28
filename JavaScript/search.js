5255f2878b3d05b50ac1a2f8dfb0244f366d7751
KasraMg <kasragithub7@gmail.com>
2023-07-28 16:48:24 +0330

    update

--- JavaScript/search.js
+++ JavaScript/search.js
@@ -12,7 +12,7 @@ window.addEventListener('load', () => {
     const loader = document.querySelector('.loader')
 
     getInfoes(`https://one-api.ir/radiojavan/?token=677668:64ae5b9d7c848&action=search&q=${param}`).then(data => {
-        console.log(data);
+         
         loader.classList.add('hidden')
         if (data.result.mp3s.length > 0) {
             data.result.mp3s.map(music => {
