270d1fe115793bc8fc0b15acb191b96fdff9123e
KasraMg <kasragithub7@gmail.com>
2023-08-16 22:19:48 +0330

    indexLinkUpdate

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -13,11 +13,11 @@ artists.slice(0, 6).map(info => (
   artistsDev.insertAdjacentHTML('beforeend',
     `
     <div class="w-full cursor-pointer">
-    <a href='artist.html?artist=${info.englishName}&type=all&page=1' class="">
+    <a href='./Src/artist.html?artist=${info.englishName}&type=all&page=1' class="">
       <img src="${info.photo}" alt="cover" class="rounded-full mx-auto " style='height:140px;width:140px'>
     </a>
 
-    <a href='artist.html?artist=${info.englishName}&type=all&page=1''  >
+    <a href='./Src/artist.html?artist=${info.englishName}&type=all&page=1''  >
       <h3 class="font-bold mt-4 text-center text-lg md:text-md text-darkBg dark:text-white">${info.name} </h3>
     </a>
 
@@ -29,7 +29,7 @@ ourPlayList.forEach(item =>
   playListContent.insertAdjacentHTML(
     "beforeend", `
         
-        <a href='mPlayList.html?type=ourPlayList&plId=${item.id}&id=${item.data[0].id}' class="w-full cursor-pointer">
+        <a href='./Src/mPlayList.html?type=ourPlayList&plId=${item.id}&id=${item.data[0].id}' class="w-full cursor-pointer">
         <div class="relative ">
           <img src=${item.data[0].photo} alt="cover" class="rounded-lg w-full h-full">
 
@@ -72,7 +72,36 @@ window.addEventListener('load', () => {
 
       resultFilter.slice(0, 8).map(music => (
         newSong.insertAdjacentHTML(
-          "beforeend", mediaHtmlTemplate(music))
+          "beforeend", `           
+          <div class="w-full">
+    <div class="relative">
+    <a  href='${music.type === 'mp3' ? `./Src/music.html` : `./Src/mVideo.html`}?artist=${music.artist}&id=${music.id}'><img  src=${music.photo} alt="cover" class="rounded-lg w-full object-cover" ${music.type === 'all' ? `style=height:16rem` : ''}></a> 
+    
+    ${music.type === 'mp3' ?
+          `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    stroke="currentColor" class="w-6 h-6 absolute top-2 right-2 text-white">
+    <path stroke-linecap="round" stroke-linejoin="round"
+    d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
+    </svg>
+    `
+          :
+          `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    stroke="currentColor" class="w-6 h-6 absolute top-2 right-2 text-white">
+    <path stroke-linecap="round"
+    d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+    </svg>
+    `
+        }
+    
+    </div>
+    <a href='${music.type === 'mp3' ? `./Src/music.html` : `./Src/mVideo.html`}?artist=${music.artist}&id=${music.id}' >
+    <h3 class="font-bold text-lg md:text-md mb-1 mt-4 text-darkBg dark:text-white"> ${music.song_farsi}</h3>
+    <h4 class="font-semibold text-sm md:text-xs text-secondText">${music.artist_farsi}</h4>
+    </a>
+    
+    </div>`)
       ))
 
 
@@ -97,7 +126,36 @@ window.addEventListener('load', () => {
 
       resultFilter.slice(0, 8).map(music => (
         specialMusics.insertAdjacentHTML(
-          "beforeend", mediaHtmlTemplate(music))
+          "beforeend", `           
+          <div class="w-full">
+    <div class="relative">
+    <a  href='${music.type === 'mp3' ? `./Src/music.html` : `./Src/mVideo.html`}?artist=${music.artist}&id=${music.id}'><img  src=${music.photo} alt="cover" class="rounded-lg w-full object-cover" ${music.type === 'all' ? `style=height:16rem` : ''}></a> 
+    
+    ${music.type === 'mp3' ?
+          `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    stroke="currentColor" class="w-6 h-6 absolute top-2 right-2 text-white">
+    <path stroke-linecap="round" stroke-linejoin="round"
+    d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
+    </svg>
+    `
+          :
+          `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    stroke="currentColor" class="w-6 h-6 absolute top-2 right-2 text-white">
+    <path stroke-linecap="round"
+    d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+    </svg>
+    `
+        }
+    
+    </div>
+    <a href='${music.type === 'mp3' ? `./Src/music.html` : `./Src/mVideo.html`}?artist=${music.artist}&id=${music.id}' >
+    <h3 class="font-bold text-lg md:text-md mb-1 mt-4 text-darkBg dark:text-white"> ${music.song_farsi}</h3>
+    <h4 class="font-semibold text-sm md:text-xs text-secondText">${music.artist_farsi}</h4>
+    </a>
+    
+    </div>`)
       ))
 
       let randomArray = createRandomArray(resultFilter.length);
@@ -106,7 +164,7 @@ window.addEventListener('load', () => {
       sliderToShow.map(music => (
         mySwiper1.insertAdjacentHTML(
           "beforeend", `
-                          <a  href='music.html?artist=${music.artist}&id=${music.id}'  class="relative swiper-slide">
+                          <a  href='./Src/music.html?artist=${music.artist}&id=${music.id}'  class="relative swiper-slide">
                           <img src='${music.photo}' class=' w-full rounded-xl'/>
                           <div class="absolute" id="textCap">
                           <p>${music.artist_farsi}</p>
@@ -161,7 +219,36 @@ window.addEventListener('load', () => {
 
       resultFilter.slice(0, 8).map(music => (
         newMusicVideo.insertAdjacentHTML(
-          "beforeend", mediaHtmlTemplate(music))
+          "beforeend", `           
+          <div class="w-full">
+    <div class="relative">
+    <a  href='${music.type === 'mp3' ? `./Src/music.html` : `./Src/mVideo.html`}?artist=${music.artist}&id=${music.id}'><img  src=${music.photo} alt="cover" class="rounded-lg w-full object-cover" ${music.type === 'all' ? `style=height:16rem` : ''}></a> 
+    
+    ${music.type === 'mp3' ?
+          `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    stroke="currentColor" class="w-6 h-6 absolute top-2 right-2 text-white">
+    <path stroke-linecap="round" stroke-linejoin="round"
+    d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
+    </svg>
+    `
+          :
+          `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    stroke="currentColor" class="w-6 h-6 absolute top-2 right-2 text-white">
+    <path stroke-linecap="round"
+    d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+    </svg>
+    `
+        }
+    
+    </div>
+    <a href='${music.type === 'mp3' ? `./Src/music.html` : `./Src/mVideo.html`}?artist=${music.artist}&id=${music.id}' >
+    <h3 class="font-bold text-lg md:text-md mb-1 mt-4 text-darkBg dark:text-white"> ${music.song_farsi}</h3>
+    <h4 class="font-semibold text-sm md:text-xs text-secondText">${music.artist_farsi}</h4>
+    </a>
+    
+    </div>`)
       ))
     }else{
       location.href='error.html'
